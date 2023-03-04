1e70da970ea5f6896039164ace343066f560c0e2
Tonin386 <tonin386@gmail.com>
2023-03-04 19:33:11 +0100

    First version of ubuntu script fully functionnal

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dave
+
+Add this line to `sudo visudo`:
+`fiddle ALL=(ALL) NOPASSWD: /sbin/iwlist`
--- measure_unix.py
+++ measure_unix.py
@@ -2,20 +2,25 @@ from rssi import RSSI_Scan, RSSI_Localizer
 import re
 import os
 import time
+import json
+from copy import deepcopy
 
-zone_label = "B208"
-zone_index = 0
+zone_label = input("Enter zone label: ")
+zone_index = int(input("Enter zone index: "))
 # networks_ssid = [] #Put the names of the networks that interest us.
-networks_address = [""] #Put the IPv6 adresses of the networks that interest us.
+networks_address = ["20:9A:7D:09:B8:F4", "E0:0E:E4:C0:98:B2", "4A:14:04:14:41:58"] #Put the IPv6 adresses of the networks that interest us.
+print("You will need to make 20 measures. Every time you will need to move, you'll be asked so.")
+position = int(input("Please input the original position. Measurement will start once you pressed enter: "))
+if position < 0:
+	position = 0
 
 def scan_networks():
 	interface = "wlp0s20f3"
 	scanner = RSSI_Scan(interface)
 
-	raw_data = scanner.getRawNetworkScan()["output"]
+	raw_data = scanner.getRawNetworkScan(True)["output"]
 	raw_cells = raw_data.decode().split("Cell")
 	raw_cells = raw_cells[1:]
-	print(len(raw_cells))
 
 	collected_data = []
 
@@ -24,9 +29,9 @@ def scan_networks():
 		network_address = re.sub(r"^.*Address: ", "", lines[0])
 		signal_level_text = re.sub("^.*Signal level=", "", lines[3])
 		signal_level_value = int(signal_level_text.replace(" dBm ", ""))
-		network_ssid = lines[5].replace("ESSID:", "").replace('"', '')
+		network_ssid = re.sub("^.*ESSID:", "", lines[5]).replace('"', '')
 
-		print("%s %d" % (network_address, signal_level_value))
+		# print("%s %d" % (network_address, signal_level_value))
 		network_data = {
 			"ssid": network_ssid,
 			"address": network_address,
@@ -36,18 +41,47 @@ def scan_networks():
 
 	return collected_data
 
-def save_data(d):
-	print("TODO")
+def save_data(data, position, acquisition):
+	path = "data/%s/%d/%d.json" % (zone_label, position, acquisition)
+	os.makedirs(os.path.dirname(path), exist_ok=True)
 
-for i in range(3):
-	data = scan_networks()
+	ordered_data = {}
 	for d in data:
-		if d["address"] in networks_address:
-			save_data(d)
+		if d["address"] in ordered_data.keys():
+			ordered_data[d["address"]]["signalStrength"].append(d["signalStrength"])
+		else:
+			ordered_data[d["address"]] = {}
+			ordered_data[d["address"]]["signalStrength"] = [d["signalStrength"]]
+			ordered_data[d["address"]]["ssid"] = d["ssid"]
 
-	if i == 2:
-		break
-	print("Waiting 5 seconds before the next measure...")
-	time.sleep(5)
+	json_string = json.dumps(ordered_data, indent=4)
+	with open(path, "w") as file:
+		file.write(json_string)
 
+def clear_data(data):
+	c_data = deepcopy(data)
+	for d in data:
+		if not d["address"] in networks_address:
+			c_data.remove(d)
 
+	return c_data
+
+for i in range(20):
+	for j in range(3):
+		acquisition_data = []
+		st = time.time()
+		for k in range(50):
+			data = scan_networks()
+			acquisition_data.extend(clear_data(data))
+			print("%d/50" % (k+1))
+
+		et = time.time()
+		elapsed = et - st
+		print("Exec time: %f seconds" % elapsed)
+		save_data(acquisition_data, position, j)
+		if j == 2:
+				break
+		print("Waiting 5 seconds before acquisition #%d..." % (j+1))
+		time.sleep(5)
+	position += 1
+	input("Press enter once you're ready to do measure #%d" % position)
